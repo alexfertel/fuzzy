88386cec56c13970e2dc5c26db681c3fe2728d60
Alexander Gonzalez <alexfertel97@gmail.com>
2019-05-16 16:49:57 -0400

    Finish paper

--- main.py
+++ main.py
@@ -15,114 +15,147 @@ from mpl_toolkits import mplot3d
 def main():
     map_control_domain = (0, 100)
     map_control_sets = {
-        'none': (m.r_func, (15, 35)),
+        'none': (m.gaussian, (0, 35)),
         'light': (m.gaussian, (32.5, 13)),
         'moderate': (m.gaussian, (50, 20)),
-        'tight': (m.gaussian, (67.5, 13)),
-        'complete': (m.l_func, (55, 75)),
-    }
-
-    fns = []
-    for label, (fn, args) in map_control_sets.items():
-        fns.append((partial(fn, *args), label))
-    plot_with_labels(fns, map_control_domain)
-
-    speed_domain = (0, 140)
-    speed_sets = {
-        'very slow': (m.r_func, (15, 20)),
-        'slow': (m.triangular, (7, 30, 50)),
-        'moderate': (m.triangular, (35, 60, 85)),
-        'fast': (m.triangular, (55, 100, 130)),
-        'very fast': (m.l_func, (90, 120)),
-    }
-
-
-    inputs = {
-        'distance to stop': Variable('distance to stop', map_control_sets, map_control_domain),
-        'speed': Variable('speed', speed_sets, speed_domain)
-    }
-
-    break_pressure_domain = (0, 100)
-    break_pressure_sets = {
-        'light': (m.r_func, (2, 25)),
-        'moderate': (m.triangular, (4, 40, 75)),
-        'hard': (m.l_func, (43, 95)),
+        'superior': (m.gaussian, (67.5, 13)),
+        'complete': (m.gaussian, (100, 25)),
     }
 
     # fns = []
     # for label, (fn, args) in map_control_sets.items():
     #     fns.append((partial(fn, *args), label))
     # plot_with_labels(fns, map_control_domain)
+
+    gold_lead_domain = (-100, 100)
+    gold_lead_sets = {
+        'losing by far': (m.gaussian, (-100, 40)),
+        'losing': (m.gaussian, (-50, 35)),
+        'losing by a little': (m.gaussian, (-25, 25)),
+        'leverage': (m.gaussian, (0, 10)),
+        'winning by a little': (m.gaussian, (25, 25)),
+        'winning': (m.gaussian, (50, 35)),
+        'winning by far': (m.gaussian, (100, 15)),
+    }
+
+    inputs = {
+        'map control': Variable('map control', map_control_sets, map_control_domain),
+        'gold lead': Variable('gold lead', gold_lead_sets, gold_lead_domain)
+    }
+
+    show_hero_domain = (0, 100)
+    show_hero_sets = {
+        'never': (m.gaussian, (0, 30)),
+        'a little': (m.gaussian, (30, 15)),
+        'moderately': (m.gaussian, (55, 25)),
+        'a while': (m.gaussian, (80, 12.5)),
+        'always': (m.gaussian, (100, 2)),
+    }
+
     # fns = []
-    # for label, (fn, args) in speed_sets.items():
-    #     fns.append((partial(fn, *args), label))
-    # plot_with_labels(fns, speed_domain)
+    # for label, (fn, args) in map_control_sets.items():
+    #     fns.append((partial(fn, *args), label.split(' ')[-1]))
+    # plot_with_labels(fns, map_control_domain)
     # fns = []
-    # for label, (fn, args) in break_pressure_sets.items():
-    #     fns.append((partial(fn, *args), label))
-    # plot_with_labels(fns, break_pressure_domain)
+    # for label, (fn, args) in gold_lead_sets.items():
+    #     fns.append((partial(fn, *args), label.split(' ')[-1]))
+    # plot_with_labels(fns, gold_lead_domain)
+    # fns = []
+    # for label, (fn, args) in show_hero_sets.items():
+    #     fns.append((partial(fn, *args), label.split(' ')[-1]))
+    # plot_with_labels(fns, show_hero_domain)
 
     outputs = {
-        'break pressure': Variable('break pressure', break_pressure_sets, break_pressure_domain)
+        'show hero': Variable('show hero', show_hero_sets, show_hero_domain)
     }
 
     # rules = [
-    #     "IF distance to stop IS close AND speed IS very slow THEN break pressure IS light",
-    #     "IF distance to stop IS close AND speed IS slow THEN break pressure IS moderate",
-    #     "IF distance to stop IS close AND speed IS moderate THEN break pressure IS hard",
-    #     "IF distance to stop IS close AND ( speed IS fast OR speed IS very fast ) THEN break pressure IS hard",
+    #     "IF map control IS close AND gold lead IS very slow THEN show hero IS light",
+    #     "IF map control IS close AND gold lead IS slow THEN show hero IS moderate",
+    #     "IF map control IS close AND gold lead IS moderate THEN show hero IS hard",
+    #     "IF map control IS close AND ( gold lead IS fast OR gold lead IS very fast ) THEN show hero IS hard",
 
-    #     "IF distance to stop IS medium AND ( speed IS very slow OR speed IS slow ) THEN break pressure IS light",
-    #     "IF distance to stop IS medium AND ( speed IS moderate OR speed IS fast ) THEN break pressure IS moderate",
-    #     "IF distance to stop IS medium AND speed IS very fast THEN break pressure IS hard",
+    #     "IF map control IS medium AND ( gold lead IS very slow OR gold lead IS slow ) THEN show hero IS light",
+    #     "IF map control IS medium AND ( gold lead IS moderate OR gold lead IS fast ) THEN show hero IS moderate",
+    #     "IF map control IS medium AND gold lead IS very fast THEN show hero IS hard",
 
-    #     "IF distance to stop IS far AND ( speed IS slow OR speed IS moderate OR speed IS very slow ) THEN break pressure IS light",
-    #     "IF distance to stop IS far AND ( speed IS fast OR speed IS very fast ) THEN break pressure IS moderate",
+    #     "IF map control IS far AND ( gold lead IS slow OR gold lead IS moderate OR gold lead IS very slow ) THEN show hero IS light",
+    #     "IF map control IS far AND ( gold lead IS fast OR gold lead IS very fast ) THEN show hero IS moderate",
     # ]
 
+
     rules = [
-        "IF distance to stop IS close AND speed IS very slow THEN break pressure IS light",
-        "IF distance to stop IS close AND speed IS slow THEN break pressure IS moderate",
-        "IF distance to stop IS close AND speed IS moderate THEN break pressure IS hard",
-        "IF distance to stop IS close AND speed IS fast THEN break pressure IS hard",
-        "IF distance to stop IS close AND speed IS very fast THEN break pressure IS hard",
+        "IF map control IS none AND gold lead IS losing by far THEN show hero IS never",
+        "IF map control IS none AND gold lead IS losing THEN show hero IS never",
+        "IF map control IS none AND gold lead IS losing by a little THEN show hero IS a little",
+        "IF map control IS none AND gold lead IS leverage THEN show hero IS a little",
+        "IF map control IS none AND gold lead IS winning by a little THEN show hero IS moderately",
+        "IF map control IS none AND gold lead IS winning THEN show hero IS moderately",
+        "IF map control IS none AND gold lead IS winning by far THEN show hero IS a while",
 
-        "IF distance to stop IS medium AND speed IS very slow THEN break pressure IS light",
-        "IF distance to stop IS medium AND speed IS slow THEN break pressure IS light",
-        "IF distance to stop IS medium AND speed IS moderate THEN break pressure IS moderate",
-        "IF distance to stop IS medium AND speed IS fast THEN break pressure IS moderate",
-        "IF distance to stop IS medium AND speed IS very fast THEN break pressure IS hard",
+        "IF map control IS light AND gold lead IS losing by far THEN show hero IS never",
+        "IF map control IS light AND gold lead IS losing THEN show hero IS a little",
+        "IF map control IS light AND gold lead IS losing by a little THEN show hero IS a little",
+        "IF map control IS light AND gold lead IS leverage THEN show hero IS moderately",
+        "IF map control IS light AND gold lead IS winning by a little THEN show hero IS moderately",
+        "IF map control IS light AND gold lead IS winning THEN show hero IS a while",
+        "IF map control IS light AND gold lead IS winning by far THEN show hero IS a while",
 
-        "IF distance to stop IS far AND speed IS very slow THEN break pressure IS light",
-        "IF distance to stop IS far AND speed IS slow THEN break pressure IS light",
-        "IF distance to stop IS far AND speed IS moderate THEN break pressure IS light",
-        "IF distance to stop IS far AND speed IS fast THEN break pressure IS moderate",
-        "IF distance to stop IS far AND speed IS very fast THEN break pressure IS moderate",
+        "IF map control IS moderate AND gold lead IS losing by far THEN show hero IS a little",
+        "IF map control IS moderate AND gold lead IS losing THEN show hero IS a little",
+        "IF map control IS moderate AND gold lead IS losing by a little THEN show hero IS moderately",
+        "IF map control IS moderate AND gold lead IS leverage THEN show hero IS a while",
+        "IF map control IS moderate AND gold lead IS winning by a little THEN show hero IS a while",
+        "IF map control IS moderate AND gold lead IS winning THEN show hero IS a while",
+        "IF map control IS moderate AND gold lead IS winning by far THEN show hero IS always",
+
+        "IF map control IS superior AND gold lead IS losing by far THEN show hero IS moderately",
+        "IF map control IS superior AND gold lead IS losing THEN show hero IS moderately",
+        "IF map control IS superior AND gold lead IS losing by a little THEN show hero IS moderately",
+        "IF map control IS superior AND gold lead IS leverage THEN show hero IS a while",
+        "IF map control IS superior AND gold lead IS winning by a little THEN show hero IS always",
+        "IF map control IS superior AND gold lead IS winning THEN show hero IS always",
+        "IF map control IS superior AND gold lead IS winning by far THEN show hero IS always",
+
+        "IF map control IS complete AND gold lead IS losing by far THEN show hero IS a little",
+        "IF map control IS complete AND gold lead IS losing THEN show hero IS moderately",
+        "IF map control IS complete AND gold lead IS losing by a little THEN show hero IS a while",
+        "IF map control IS complete AND gold lead IS leverage THEN show hero IS a while",
+        "IF map control IS complete AND gold lead IS winning by a little THEN show hero IS always",
+        "IF map control IS complete AND gold lead IS winning THEN show hero IS always",
+        "IF map control IS complete AND gold lead IS winning by far THEN show hero IS always",
     ]
 
-    inference_system = Fuzzy(inputs, outputs, Default(), rules, truncate, smallestofmaximum)
-    # crisp_output = inference_system.infer([("distance to stop", 33.43), ("speed", 140)])
+    inference_system = Fuzzy(inputs, outputs, Default(), rules, truncate, bisector)
+    # crisp_output = inference_system.infer([("map control", 33.43), ("gold lead", 15)])
     # print(crisp_output)
 
     n = 100
+    s = 20
     xa, xb = map_control_domain
-    x_val = [x for x in np.arange(xa, xb, (xb - xa) / n) for _ in range(10)]
+    x_val = [x for x in np.arange(xa, xb, (xb - xa) / n) for _ in range(s)]
    
-    ya, yb = speed_domain   
-    y_val = [y for y in np.arange(ya, yb, (yb - ya) / n)] * 10
-    z_val = [inference_system.infer([("distance to stop", x_val[i]), ("speed", y_val[i])])["break pressure"] for i in range(len(x_val))]
+    ya, yb = gold_lead_domain   
+    y_val = [y for y in np.arange(ya, yb, (yb - ya) / n)] * s
+    z_val = [inference_system.infer([("map control", x_val[i]), ("gold lead", y_val[i])])["show hero"] for i in range(len(x_val))]
 
     for x, y, z in zip(x_val, y_val, z_val):
         print(x, y, z)
 
-    ax = plt.axes(projection='3d')
-    ax.scatter3D(x_val, y_val, z_val)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+    for i in range(s):
+        c = colors[i % len(colors)]
+        for x, y, z in zip(x_val[i * n: i * n + n], y_val[i * n: i * n + n], z_val[i * n: i * n + n]):
+            ax.scatter(x, y, z, c=c)
+            
+    ax.set_xlabel('Control del mapa')
+    ax.set_ylabel('Ventaja en oro')
+    ax.set_zlabel('Mostrar el héroe')
 
-    ax.set_xlabel('Distancia al pare')
-    ax.set_ylabel('Velocidad')
-    ax.set_zlabel('Presión aplicada')
-
-    # plt.savefig(f'paper/images/{name}.png', bbox_inches='tight')
+    # plt.savefig(f'paper/images/{name}.png', bbox_inches='superior')
     plt.show()
 
 
